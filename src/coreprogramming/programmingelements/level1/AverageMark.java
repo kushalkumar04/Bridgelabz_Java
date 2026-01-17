588a5178b3bb84d29dd19b2bad1fd884685624bb
Kushal Kumar <gaddamkushal321@gmail.com>
2026-01-17 14:22:05 +0530

    Added Average Mark file

---
+++ src/coreprogramming/programmingelements/level1/AverageMark.java
@@ -0,0 +1,19 @@
+package coreprogramming.programmingelements.level1;
+
+/*
+ * This class calculates and prints the average of marks
+ * obtained in three subjects: Mathematics, Physics, and Chemistry.
+ */
+public class AverageMark {
+
+    public static void main(String[] args) {
+
+        // Store marks of three subjects
+        int maths = 94;
+        int phy = 95;
+        int chem = 96;
+
+        // Calculate and print the average of the three marks
+        System.out.println((maths + phy + chem) / 3);
+    }
+}
